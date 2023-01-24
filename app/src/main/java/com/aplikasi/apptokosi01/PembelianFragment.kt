d6d6cb1971dfb6511ce06d0226d2dcb129f60497
heiNANDA <faizalarnanda@students.amikom.ac.id>
2023-01-24 22:37:51 +0700

    upload

---
+++ app/src/main/java/com/aplikasi/apptokosi01/PembelianFragment.kt
@@ -0,0 +1,20 @@
+package com.aplikasi.apptokosi01
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PembelianFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pembelian, container, false)
+    }
+
+
+}
